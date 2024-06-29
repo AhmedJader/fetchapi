0a662550cbf6d7ff46c592217c5ba02bbc85e27c
Ahmed <Ahmedjader837@gmail.com>
2024-06-28 22:08:33 -0400

    created onclick function fetchdata

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 import { url } from './config.js';
 
-fetch(url)
-    .then(response => console.log(response))
+function fetchData(){
+    fetch(url)
+    .then(response => {
+        if(!response.ok){
+            throw new Error("could not fetch resource!");
+        }
+        return response.json();
+    })
+    .then(data => console.log(data))
     .catch(error => console.error(error));
+}
